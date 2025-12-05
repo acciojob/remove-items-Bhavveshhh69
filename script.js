070bb609db6d963da3d3448ea21ff1408964a82d
Bhavesh Suresh <149222281+Bhavveshhh69@users.noreply.github.com>
2025-12-05 21:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-//your JS code here. If required.
 function removeColor() {
-  const select = document.getElementById("colorSelect");
-  select.remove(select.selectedIndex);
+    const select = document.getElementById("colorSelect");
+    const index = select.selectedIndex;
+
+    if (index !== -1) {
+        select.remove(index);
+    }
 }
